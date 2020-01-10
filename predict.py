c8103384108297d40b5a3e73e7bbaf4d3e212d6a
Anton Boksha <antonboksha@gmail.com>
2020-01-10 12:29:16 +0200

    various fixes of predictions

--- predict.py
+++ predict.py
@@ -1,21 +1,24 @@
 import tensorflow as tf
 import numpy as np
 import cv2
+import pathlib
 
 IMG_WIDTH = 256
 IMG_HEIGHT = 256
+TRAIN_DIR = pathlib.Path('train_imgs')
+labels = [folder.name for folder in TRAIN_DIR.glob('*')]
 
 model = tf.keras.models.load_model('dogs_and_cats_model.h5')
 model.summary()
 
-img_cat = cv2.imread('cat_small.jpg')
-img_cat = (np.expand_dims(img_cat, 0))
-print(img_cat)
-prediction1 = model.predict(img_cat)
-print(prediction1)
-
-img_dog = cv2.imread('dog_small.jpg')
-img_dog = (np.expand_dims(img_dog, 0))
-print(img_dog)
-prediction2 = model.predict(img_dog)
-print(prediction2)
+img = cv2.imread('dog.jpg')
+img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT), interpolation=cv2.INTER_NEAREST)
+# cv2.imshow('Image', img)
+# cv2.waitKey(0)
+img = np.reshape(img, [1, IMG_HEIGHT, IMG_WIDTH, 3])
+img = img / 255.0
+prediction1 = model.predict(img)
+print(prediction1[0])
+index = prediction1.argmax(axis=-1)[0]
+predicted_label = sorted(labels)[index]
+print(predicted_label)
--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import os
 
 # Basically, default values are the same as here
 BATCH_SIZE = 32
-EPOCHS = 15
+EPOCHS = 5
 IMG_WIDTH = 256
 IMG_HEIGHT = 256
 AUTOTUNE = tf.data.experimental.AUTOTUNE
@@ -14,7 +14,8 @@ VAL_DIR = pathlib.Path('validation_imgs')
 
 train_image_count = len(list(TRAIN_DIR.glob('*/*.jpg')))
 validation_image_count = len(list(VAL_DIR.glob('*/*.jpg')))
-print(f'TRAIN IMAGES - {train_image_count}\nVALIDATION IMAGES - {validation_image_count}')
+print(f'TRAIN IMAGES - {train_image_count}')
+print(f'VALIDATION IMAGES - {validation_image_count}')
 
 CLASS_NAMES = np.array([folder.name for folder in TRAIN_DIR.glob('*')])
 print(f'CLASSES - {CLASS_NAMES}')
@@ -22,49 +23,57 @@ print(f'CLASSES - {CLASS_NAMES}')
 train_list_ds = tf.data.Dataset.list_files(str(TRAIN_DIR/'*/*'))
 validation_list_ds = tf.data.Dataset.list_files(str(VAL_DIR/'*/*'))
 
+
 def get_label(file_path):
-  parts = tf.strings.split(file_path, os.path.sep)
-  return parts[-2] == CLASS_NAMES
+    parts = tf.strings.split(file_path, os.path.sep)
+    return parts[-2] == CLASS_NAMES
+
 
 def decode_img(img):
-  img = tf.image.decode_jpeg(img, channels=3)
-  img = tf.image.convert_image_dtype(img, tf.float32)
-  return tf.image.resize(img, [IMG_WIDTH, IMG_HEIGHT])
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    return tf.image.resize(img, [IMG_WIDTH, IMG_HEIGHT])
+
 
 def process_path(file_path):
-  label = get_label(file_path)
-  img = tf.io.read_file(file_path)
-  img = decode_img(img)
-  return img, label
+    label = get_label(file_path)
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
+    return img, label
+
 
 train_labeled_ds = train_list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-validation_labeled_ds = validation_list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+validation_labeled_ds = validation_list_ds.map(
+    process_path, num_parallel_calls=AUTOTUNE)
+
 
 def prepare_for_training(ds, cache=True, shuffle_buffer_size=1000):
-  if cache:
-    if isinstance(cache, str):
-      ds = ds.cache(cache)
-    else:
-      ds = ds.cache()
-  ds = ds.shuffle(buffer_size=shuffle_buffer_size)
-  ds = ds.repeat()
-  ds = ds.batch(BATCH_SIZE)
-  ds = ds.prefetch(buffer_size=AUTOTUNE)
-  return ds
+    if cache:
+        if isinstance(cache, str):
+            ds = ds.cache(cache)
+        else:
+            ds = ds.cache()
+    ds = ds.shuffle(buffer_size=shuffle_buffer_size)
+    ds = ds.repeat()
+    ds = ds.batch(BATCH_SIZE)
+    ds = ds.prefetch(buffer_size=AUTOTUNE)
+    return ds
+
 
 train_ds = prepare_for_training(train_labeled_ds)
 validation_ds = prepare_for_training(validation_labeled_ds)
 
 model = tf.keras.Sequential([
-  tf.keras.layers.Conv2D(16, 3, activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
-  tf.keras.layers.MaxPool2D(),
-  tf.keras.layers.Conv2D(32, 3, activation='relu'),
-  tf.keras.layers.MaxPool2D(),
-  tf.keras.layers.Conv2D(64, 3, activation='relu'),
-  tf.keras.layers.MaxPool2D(),
-  tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dense(len(list(CLASS_NAMES)), activation='softmax')
+    tf.keras.layers.Conv2D(16, 3, activation='relu',
+                           input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+    tf.keras.layers.MaxPool2D(),
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.MaxPool2D(),
+    tf.keras.layers.Conv2D(64, 3, activation='relu'),
+    tf.keras.layers.MaxPool2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(len(list(CLASS_NAMES)), activation='softmax')
 ])
 
 model.compile(optimizer='adam',
